d34257a4ee638292043f6eacdd8c24618cbc8a25
onojaMatthew <onojamatthew59@gmail.com>
2021-07-14 10:40:07 +0100

    added new updates

--- src/controllers/churches/router.js
+++ src/controllers/churches/router.js
@@ -1,13 +1,15 @@
 import express from "express";
+import { grantAccess } from "../../middleware/access";
+import { verifyToken } from "../../middleware/auth";
 import { churchDetails, churchList, createChurch, deleteChurch, updateChurch } from "./controller";
 
 
 const router = express.Router();
 
-router.post("/church/new", createChurch);
-router.get("/church/all", churchList);
-router.get("/church/detail/:churchId", churchDetails);
-router.put("/church/update/:churchId", updateChurch);
-router.delete("/church/delete/:churchId", deleteChurch);
+router.post("/church/new", verifyToken, grantAccess("createAny", "super admin"), createChurch);
+router.get("/church/all", verifyToken, grantAccess("readAny", "super admin"), churchList);
+router.get("/church/detail/:churchId", verifyToken, grantAccess("readOwn", "church"), churchDetails);
+router.put("/church/update/:churchId", verifyToken, grantAccess("updateOnw", "church"), updateChurch);
+router.delete("/church/delete/:churchId", verifyToken, grantAccess("deleteAny", "super admin"), deleteChurch);
 
 export default router;
