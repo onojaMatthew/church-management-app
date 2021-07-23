b9a06d58aac751a6d67a8746cc2b0e44c151b668
onojaMatthew <onojamatthew59@gmail.com>
2021-07-23 10:12:18 +0100

    added

--- src/config/db.js
+++ src/config/db.js
@@ -24,18 +24,19 @@ const mongoOptions = {
   useFindAndModify: false,
   autoIndex: true,
   poolSize: 10,
-  // bufferMaxEntries: 0,
+  bufferMaxEntries: 0,
   connectTimeoutMS: 30000,
   socketTimeoutMS: 30000,
 };
 
 const connect = () => mongoose.createConnection(db_url, mongoOptions);
 
-const connectToMongoDB = () => {
-  const db = connect(db_url);
-  db.then(() => {
+const connectToMongoDB = async () => {
+  const db = await connect(db_url);
+  db.on("open", () => {
     winston.info(`Connection to database established with ${JSON.stringify(db_url)}`);
-  }).catch(err => {
+  })
+  db.on("error", (err) => {
     winston.error(`Failed to connect to database. Error: ${err.message}`);
   })
   
