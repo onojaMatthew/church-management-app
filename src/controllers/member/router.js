4f6df93dc49a4a8bbf5d83714f7819cf511d9995
onojaMatthew <onojamatthew59@gmail.com>
2021-07-24 18:04:44 +0100

    new updates

--- src/controllers/member/router.js
+++ src/controllers/member/router.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { assignOffice, createMember, deleteMember, getMembers, memberDetails, updateMember } from "./controller";
+import { verifyToken } from "../../middleware/auth";
+import { } from "../../middleware/access";
 
 const router = express.Router();
 
-router.post("/member/new", createMember);
-router.get("/member/all/:church", getMembers);
-router.get("/member/details/:member/:church", memberDetails);
-router.put("/member/update/:member/:church", updateMember);
-router.put("/member/assign_office/:member/:church", assignOffice);
-router.delete("/member/delete/:church/:memberId", deleteMember);
+router.post("/member/new", verifyToken, grantAccess("createOwn", "church"), createMember);
+router.get("/member/all/:church", verifyToken, grantAccess("createOwn", "church"), getMembers);
+router.get("/member/details/:member/:church", verifyToken, grantAccess("createOwn", "church"), memberDetails);
+router.put("/member/update/:member/:church", verifyToken, grantAccess("createOwn", "church"), updateMember);
+router.put("/member/assign_office/:member/:church", verifyToken, grantAccess("createOwn", "church"), assignOffice);
+router.delete("/member/delete/:church/:memberId", verifyToken, grantAccess("createOwn", "church"), deleteMember);
 
 export default router;
