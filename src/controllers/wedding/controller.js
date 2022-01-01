d3145f4ab47929e1602a122bec5b7ec8d2d08c87
onojaMatthew <onojamatthew59@gmail.com>
2022-01-01 02:37:43 +0100

    added newupdates

--- src/controllers/wedding/controller.js
+++ src/controllers/wedding/controller.js
@@ -3,13 +3,14 @@ import { weddingSchema } from "../../models/wedding";
 import { success, error } from "../../config/response";
 
 export const create = async (req, res) => {
-  const { groom_first_name, groom_last_name, groom_phone_number, bride_first_name, bride_last_name, bride_phone_number, venue,date,lead_pastor, } = req.body;
+  const { groom_first_name, groom_last_name, groom_phone_number, wedding_picture, bride_first_name, bride_last_name, bride_phone_number, venue,date,lead_pastor, } = req.body;
   const { church } = req.query;
+
   try {
     const Wedding = await getModelByChurch(church, "Wedding", weddingSchema);
     const itExists = await Wedding.findOne({ "groom.phone": groom_phone_number, "bride.phone": bride_phone_number });
     if (itExists) return res.json(success("Wedding event already exists", itExists, res.statusCode ));
-    const wedding_picture = req.files.wedding_picture && req.files.wedding_picture[0].location;
+    
     let newWedding = new Wedding({
       "groom.first_name": groom_first_name,
       "groom.last_name": groom_last_name,
@@ -44,21 +45,21 @@ export const getWeddingList = async (req, res) => {
 
 export const updateWedding = async (req, res) => {
   const { church } = req.query;
+
   try {
     const Wedding = await getModelByChurch(church, "Wedding", weddingSchema);
-    const wedding_picture = req.files.wedding_picture && req.files.wedding_picture[0].location;
     let wedding = await Wedding.findById({ _id: req.body.id });
-    if (req.body.groom_first_name) wedding.groom_first_name = req.body.groom_first_name
+    if (req.body.groom_first_name) wedding.groom.first_name = req.body.groom_first_name
     if (req.body.groom_last_name) wedding.groom_last_name = req.body.groom_last_name
-    if (req.body.groom_phone_number) wedding.groom_phone_number = req.body.groom_phone_number
-    if (req.body.bride_first_name) wedding.groom_first_name = req.body.bride_first_name;
-    if (req.body.bride_last_name) wedding.bride_last_name = req.body.bride_last_name
-    if (req.body.bride_phone_number) wedding.bride_phone_number = req.body.bride_phone_number
-    if (req.body.bride_phone_number) wedding.bride_phone_number = req.body.bride_phone_number
+    if (req.body.groom_phone_number) wedding.gro.m.phone_number = req.body.groom_phone_number
+    if (req.body.bride_first_name) wedding.groom.first_name = req.body.bride_first_name;
+    if (req.body.bride_last_name) wedding.bride.last_name = req.body.bride_last_name
+    if (req.body.bride_phone_number) wedding.bride.phone_number = req.body.bride_phone_number
+    if (req.body.bride_phone_number) wedding.bride.phone_number = req.body.bride_phone_number
     if (req.body.venue) wedding.venue = req.body.venue;
     if (req.body.date) wedding.date = req.body.date;
     if (req.body.lead_pastor) wedding.lead_pastor = req.body.lead_pastor;
-    if (wedding_picture) wedding.wedding_picture = wedding_picture;
+    if (req.body.wedding_picture) wedding.wedding_picture = req.body.wedding_picture;
     wedding = await wedding.save();
     if (!wedding) return res.status(404).json(success("Event not found", wedding, res.statusCode));
     return res.json(success("Event updated", wedding, res.statusCode));
--- src/controllers/wedding/router.js
+++ src/controllers/wedding/router.js
@@ -1,36 +1,12 @@
 import express from "express";
 import { grantAccess } from "../../middleware/access";
 import { verifyToken } from "../../middleware/auth";
-import { upload } from "../../services/uploader";
 import { create, deleteWedding, getWeddingList, updateWedding } from "./controller";
 
 const router = express.Router();
 
-router.post("/wedding/new", verifyToken, grantAccess("createOwn", "branch church"), upload.fields([
-  { name: "groom_first_name", maxCount: 1 }, 
-  { name: "groom_last_name", maxCount: 1 }, 
-  { name: "groom_phone_number", maxCount: 1 }, 
-  { name: "bride_first_name", maxCount: 1 }, 
-  { name: "bride_last_name", maxCount: 1 }, 
-  { name: "bride_phone_number", maxCount: 1 },
-  { name: "venue", maxCount: 1 },
-  { name: "date", maxCount: 1 },
-  { name: "lead_pastor", maxCount: 1 },
-  { name: "wedding_picture", maxCount: 1 }
-]), create);
+router.post("/wedding/new", verifyToken, grantAccess("createOwn", "branch church"), create);
 router.get("/wedding/all", verifyToken, grantAccess("readOwn", "branch church"), getWeddingList);
-router.put("/wedding/update", verifyToken, grantAccess("updateOwn", "branch church"), upload.fields([
-  { name: "groom_first_name", maxCount: 1 }, 
-  { name: "groom_last_name", maxCount: 1 }, 
-  { name: "groom_phone_number", maxCount: 1 }, 
-  { name: "bride_first_name", maxCount: 1 }, 
-  { name: "bride_last_name", maxCount: 1 }, 
-  { name: "bride_phone_number", maxCount: 1 },
-  { name: "venue", maxCount: 1 },
-  { name: "date", maxCount: 1 },
-  { name: "lead_pastor", maxCount: 1 },
-  { name: "wedding_picture", maxCount: 1 },
-  { name: "id", maxCount: 1 },
-]), updateWedding);
+router.put("/wedding/update", verifyToken, grantAccess("updateOwn", "branch church"), updateWedding);
 router.delete("/wedding/delete", verifyToken, grantAccess("deleteOwn", "branch church"), deleteWedding);
 export default router;
