33367408de488891314fc03e5948c15444bc06b0
onojaMatthew <onojamatthew59@gmail.com>
2022-02-07 22:38:48 +0100

    added new updates

--- src/controllers/baptism/controller.js
+++ src/controllers/baptism/controller.js
@@ -62,3 +62,85 @@ export const delete_baptism = async (req, res) => {
     return res.status(400).json(error(err.message, res.statusCode));
   }
 }
+
+export const search = async (req, res) => {
+  const { church, searchTerm } = req.query
+  try {
+    const Baptism = await getModelByChurch(church, "Baptism", baptismSchema);
+    const baptism = await Baptism.aggregate([{ $match: {
+      $or: [
+        { first_name: {
+            $regex: searchTerm,
+            $options: "i"
+          }
+        },
+          { last_name: {
+            $regex: searchTerm,
+            $options: "i"
+          }
+        },
+        { 
+          venue: {
+            $regex: searchTerm,
+            $options: "i"
+          },
+        },
+        { 
+          date: {
+            $regex: searchTerm,
+            $options: "i"
+          },
+        },
+        { 
+          teacher: {
+            $regex: searchTerm,
+            $options: "i"
+          },
+        },
+        { 
+          administrating_pastor: {
+            $regex: searchTerm,
+            $options: "i"
+          },
+        },
+        { 
+          phone: {
+            $regex: searchTerm,
+            $options: "i"
+          },
+        },
+      ]
+    }}]);
+
+    return res.json(success("Success", baptism, res.statusCode));
+  } catch (err) {
+    return res.status(400).json(error(err.message, res.statusCode));
+  }
+}
+
+export const filter = async (req, res) => {
+  const { time_range, church } = req.query;
+  try {
+    const time_data = time_range.split(" ");
+    const time_length = Number(time_data[0]);
+    const time_param = time_data[1];
+    const date = new Date();
+    let date_ago;
+
+    if (time_param === "hours") {
+      date_ago = date.setDate(date.getHours() - time_length);
+    } else if (time_param === "days") {
+      date_ago = date.setDate(date.getDate() - time_length);
+    } else if (time_param === "weeks") {
+      date_ago = date.setDate(date.getDate() - (time_length * 7));
+    } else if (time_param === "months") {
+      date_ago = date.setDate(date.getDate() - (time_length * 30));
+    }
+
+    const Baptism = await getModelByChurch(church, "Baptism", baptismSchema);
+    const baptism = await Baptism.find({ createdAt: { $gte: date_ago }});
+    return res.json(success("Success", baptism, res.statusCode));
+  } catch (err) {
+    return res.status(400).json(error(err.message, res.statusCode));
+  }
+}
--- src/controllers/baptism/router.js
+++ src/controllers/baptism/router.js
@@ -1,17 +1,17 @@
 import express from "express";
+import { verifyToken } from "../../middleware/auth";
 import { baptism_validator } from "../../validation/birthday";
-import { delete_baptism } from "./controller";
-import { update_baptism } from "./controller";
-import { baptism_details } from "./controller";
-import { baptism_list } from "./controller";
-import { create_baptism } from "./controller";
+import { delete_baptism, filter, search } from "./controller";
+import { update_baptism, baptism_details, baptism_list, create_baptism } from "./controller";
 
 const router = express.Router();
 
-router.post("/baptism/new", baptism_validator, create_baptism);
-router.get("/baptism/list", baptism_list);
-router.get("/baptism/details", baptism_details);
-router.put("/baptism/update", update_baptism);
-router.delete("/delete", delete_baptism);
+router.post("/baptism/new", verifyToken, baptism_validator, create_baptism);
+router.get("/baptism/list", verifyToken, baptism_list);
+router.get("/baptism/details", verifyToken, baptism_details);
+router.put("/baptism/update", verifyToken, update_baptism);
+router.delete("/baptism/delete", verifyToken, delete_baptism);
+router.get("/baptism/search", verifyToken, search);
+router.get("/baptism/filter", verifyToken, filter)
 
 export default router;
