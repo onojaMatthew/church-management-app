2845bb70c62d50af2bbca8121c619ee83b4cd71c
onojaMatthew <onojamatthew59@gmail.com>
2022-08-31 01:27:31 +0100

    new changes

--- src/config/db.js
+++ src/config/db.js
@@ -19,7 +19,7 @@ const mongoOptions = {
 
 const connect = () => mongoose.createConnection(db_url, mongoOptions);
 const connectToMongoDB = () => {
-  const db = connect("mongodb+srv://ticket:kq1IL4UHiz8l4qN6@ticket.6z9ee.mongodb.net/nca");
+  const db = connect("db_url");
   db.on("open", () => {
     winston.info(`Connection to database established with ${JSON.stringify(db_url)}`);
   })
